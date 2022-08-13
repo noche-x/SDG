110cb8bbf8dfc0af2343dadf7deb10334eb7d719
noche <micheal4826@tuta.io>
2022-08-13 16:16:08 +0300

    Make example data generator not delete every file
    Signed-off-by: noche <micheal4826@tuta.io>

--- generateExampleData.py
+++ generateExampleData.py
@@ -3,20 +3,6 @@ import json
 import os
 import sys
 
-agreement = input("WARNING!\nThis script will remove every file in the current directory and generate new ones\nAre you sure you are in the plotData folder?\n(Y/n): ")
-if agreement != "y" and len(agreement) != 0:
-    exit()
-
-print("Proceeding...")
-
-selfFileName = sys.argv[0]
-print(f"Removing files except {selfFileName}")
-
-for root, subFolders, files in os.walk("."):
-    for file in files:
-        if file != selfFileName:
-            os.remove(file)
-
 dataCount = random.randint(300, 600)
 print(f"Generating {dataCount} files with random data")
 
